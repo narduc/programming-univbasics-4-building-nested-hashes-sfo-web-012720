4e68910613fe9925af0abd402a33ac07a80cf23f
Radniel Padron <radnielpadron@gmail.com>
2020-01-22 16:24:03 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	# {}
+	monopoly = { :railroads => {} }
 end
 
 def monopoly_with_second_tier
@@ -9,6 +10,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  	monopoly = { :railroads => { :pieces => 4 } }
 end
 
 def monopoly_with_third_tier
@@ -16,6 +18,20 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+    monopoly = { :railroads => { 
+      :names => {
+            :reading_railroad => {},
+            :pennsylvania_railroad => {},
+            :b_and_o_railroad => {},
+            :shortline_railroad => {}
+                },
+      :pieces => 4, 
+      :rent_in_dollars => { 
+            :one_piece_owned  => 25,
+            :two_pieces_owned => 50,
+            :three_pieces_owned => 100,
+            :four_pieces_owned  => 200,
+           } } }  
 end
 
 def monopoly_with_fourth_tier
@@ -23,4 +39,27 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  ###########################
+  
+  # describe "the monopoly_with_fourth_tier method builds on the hash returned by monopoly_with_third_tier, but updates the Hash so that each of the :railroads :names keys points to a Hash with a key \"mortgage_value\" that is a String with a $-sign and an Integer after it" do
+
+  #   it "reports reading_railroad's mortgage value as $100" do
+  #     expect(monopoly_with_fourth_tier[:railroads][:names][:reading_railroad]["mortgage_value"]).to eq("$100")
+
+    monopoly = { :railroads => { 
+      :names => {
+            :reading_railroad => { "mortgage_value" => "$100" },
+            :pennsylvania_railroad => { "mortgage_value" => "$200" },
+            :b_and_o_railroad => { "mortgage_value" => "$400" },
+            :shortline_railroad => { "mortgage_value" => "$800" }
+                },
+      :pieces => 4, 
+      :rent_in_dollars => { 
+            :one_piece_owned  => 25,
+            :two_pieces_owned => 50,
+            :three_pieces_owned => 100,
+            :four_pieces_owned  => 200,
+           } } }  
+  
 end
